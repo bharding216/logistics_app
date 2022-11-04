364c2c40e2ba2c4e0340352b41929efbc9e952a9
Brandon Harding <brandonaharding@gmail.com>
2022-11-04 13:39:13 -0500

    moving db

--- project/__init__.py
+++ project/__init__.py
@@ -7,11 +7,6 @@ from flask_mysqldb import MySQL
 import pymysql, cryptography
 
 
-# Old SQLite db connection:
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-
-# New MySQL db connection:
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:' + db['mysql_password'] + '@localhost/appointments'
 
 # General MySQL config format:
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@server/db'
@@ -20,23 +15,26 @@ import pymysql, cryptography
 # Initialize the db
 # db = SQLAlchemy(app)
 db = SQLAlchemy()
-DB_NAME = 'carbon.db'
+#DB_NAME = 'carbon.db'
 
 
 def create_app():
     app = Flask(__name__)
 
-    # db = yaml.full_load(open('db.yaml'))
-    # app.config['MYSQL_HOST'] = db['mysql_host']
-    # app.config['MYSQL_USER'] = db['mysql_user']
-    # app.config['MYSQL_PASSWORD'] = db['mysql_password']
-    # app.config['MYSQL_DB'] = db['mysql_db']
+    db = yaml.full_load(open('db.yaml'))
+    app.config['MYSQL_HOST'] = db['mysql_host']
+    app.config['MYSQL_USER'] = db['mysql_user']
+    app.config['MYSQL_PASSWORD'] = db['mysql_password']
+    app.config['MYSQL_DB'] = db['mysql_db']
 
-    # app.config['SECRET_KEY'] = db['secret_key']
+    app.config['SECRET_KEY'] = db['secret_key']
 
 
-    app.config['SECRET_KEY'] = "secret key"
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    #app.config['SECRET_KEY'] = "secret key"
+    #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    # New MySQL db connection:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:' + db['mysql_password'] + '@localhost/appointments'
+
     db.init_app(app)
 
     from .views import views
@@ -46,7 +44,7 @@ def create_app():
     app.register_blueprint(auth, url_prefix="/")
 
     from .models import users
-    create_database(app)
+    #create_database(app)
 
     login_manager = LoginManager()
     # If the user is not logged in, redirect 
@@ -60,6 +58,6 @@ def create_app():
 
     return app
 
-def create_database(app):
-    if not path.exists("project/" + DB_NAME):
-        db.create_all(app=app)
+#def create_database(app):
+#    if not path.exists("project/" + DB_NAME):
+#        db.create_all(app=app)
